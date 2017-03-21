e34121155a05a4bcf5b528a7dfceed0acd0a6965
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2017-03-21 16:03:19 +0100

    Section prefab and Students prefabs

---
+++ paperplane/New Unity Project/Assets/SectionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionController : MonoBehaviour {
+
+
+    List<Vector3> positions;
+    public GameObject SitdownStudent;
+    public GameObject StandupStudent;
+
+	// Use this for initialization
+	void Start () {
+        this.positions = new List<Vector3>();
+        var positionObjects = GameObject.FindGameObjectsWithTag("StudentPosition");
+        foreach(var position in positionObjects)
+        {
+            positions.Add(position.transform.position);
+        }
+
+        Debug.Log(positions.ToString());
+
+        InitStudents(new int[] {1,0,1,0,1,0,1});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InitStudents(int[] values)
+    {
+        for(int i=0; i<values.Length; i++)
+        {
+            var typeStudent = values[i] == 0 ? SitdownStudent : StandupStudent; 
+            var obj = Instantiate(typeStudent) as GameObject;
+
+            obj.transform.position = positions[i];
+            obj.transform.Translate(0, obj.GetComponent<Renderer>().bounds.size.y/2, 0);
+        }
+    }
+}
