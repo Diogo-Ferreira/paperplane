79d1e0b029403317fb087ecc826865b4d689a956
LumenH <aurelie.debrot@he-arc.ch>
2017-03-28 16:37:29 +0200

    some easing for the plane

--- paperplane/New Unity Project/Assets/Scripts/path.cs
+++ paperplane/New Unity Project/Assets/Scripts/path.cs
@@ -9,47 +9,98 @@ public class path : MonoBehaviour {
     public GameObject Right;
     public GameObject Middle;
 
+    public bool floatup;
+
 	// Use this for initialization
 	void Start () {
-		
+        floatup = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //go to z axe
         Vector3 pos = this.transform.position;
         pos.z += (float)0.1;
         this.transform.position = pos;
 
+        //floating
+        if (floatup)
+        {
+            floatingup();
+        }
+        else if (!floatup)
+        {
+            floatingdown();
+        }
+
+        //rail change
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             if(this.transform.position.x == Middle.transform.position.x)
             {
-                Vector3 position = this.transform.position;
+                /*Vector3 position = this.transform.position;
                 position.x = Right.transform.position.x;
+                this.transform.position = position;*/
+                Vector3 position = this.transform.position;
+                position.x = Mathf.Lerp(position.x, Right.transform.position.x, 1f);
                 this.transform.position = position;
+
             }
             else if(this.transform.position.x == Left.transform.position.x)
             {
-                Vector3 position = this.transform.position;
+                /*Vector3 position = this.transform.position;
                 position.x = Middle.transform.position.x;
+                this.transform.position = position;*/
+                Vector3 position = this.transform.position;
+                position.x = Mathf.Lerp(position.x, Middle.transform.position.x, 1f);
                 this.transform.position = position;
             }
-            
+
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if(this.transform.position.x == Middle.transform.position.x)
             {
-                Vector3 position = this.transform.position;
+                /*Vector3 position = this.transform.position;
                 position.x = Left.transform.position.x;
+                this.transform.position = position;*/
+                Vector3 position = this.transform.position;
+                position.x = Mathf.Lerp(position.x, Left.transform.position.x, 1f);
                 this.transform.position = position;
             }
             else if(this.transform.position.x == Right.transform.position.x)
             {
-                Vector3 position = this.transform.position;
+                /*Vector3 position = this.transform.position;
                 position.x = Middle.transform.position.x;
+                this.transform.position = position;*/
+                Vector3 position = this.transform.position;
+                position.x = Mathf.Lerp(position.x, Middle.transform.position.x, 1f);
                 this.transform.position = position;
             }
         }
 	}
+
+    void floatingup()
+    {
+        Vector3 pos = transform.position;
+        pos.y +=(float) 1.5 * Time.deltaTime;//Facteur à changer potentiellement
+        transform.position = pos;
+
+       // yield return new WaitForSeconds((float)0.5);
+
+        floatup = false;
+       
+    }
+
+    void floatingdown()
+    {
+        Vector3 pos = transform.position;
+        pos.y -= (float)1.5 * Time.deltaTime;//Facteur à changer potentiellement
+        transform.position = pos;
+
+        //yield return new WaitForSeconds((float)0.5);
+
+        floatup = true;
+    }
+
 }
