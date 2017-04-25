2fb1d6cc9dcb1bc39d9745eac097aabc1d99ee1b
LumenH <aurelie.debrot@he-arc.ch>
2017-04-25 15:13:09 +0200

    gameover

---
+++ paperplane/New Unity Project/Assets/Scripts/GameOver.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject player;
+    public path path;
+    private Text[] GOText;
+    public Button ReTryButton;
+
+	// Use this for initialization
+	void Start () {
+        GOText = FindObjectsOfType<Text>();
+        ReTryButton = FindObjectOfType<Button>();
+        foreach(Text g in GOText)
+        {
+            g.enabled = false;
+        }
+        ReTryButton.gameObject.SetActive(false);
+        path = player.GetComponent<path>();
+
+        ReTryButton.onClick.AddListener(retryOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!path.isAlive)
+        {
+            foreach(Text g in GOText)
+            {
+                g.enabled = true;
+            }
+
+            ReTryButton.gameObject.SetActive(true);
+            //Time.timeScale = 0;
+        }
+	}
+
+    void retryOnClick()
+    {
+        Debug.Log("Click");
+        //Restart the game
+        SceneManager.LoadScene("myscene", LoadSceneMode.Single);
+    }
+}
